0922d38beafd68de4c8ffcb8f9b608a9d6b7895c
skc4365 <50658047+skc4365@users.noreply.github.com>
2026-06-18 13:40:30 +0900

    예제4 추가

--- point/query.sql
+++ point/query.sql
@@ -1,3 +1,5 @@
+-- ex3 방명록 -------------------
+
 -- 1. 데이터베이스 생성 (이미 있으면 생략)
 --CREATE DATABASE hrd;
 
@@ -14,3 +16,19 @@ INSERT INTO guestbook (name, message)
 VALUES ('선생님', '방명록 테스트입니다!');
 
 select * from guestbook;
+
+-- ex4 로그인(세션) -------------------
+
+-- 2. 테이블 생성
+CREATE TABLE users (
+    id       SERIAL PRIMARY KEY,
+    userid   VARCHAR(50)  UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    username VARCHAR(50)  NOT NULL
+);
+
+-- 3. 테스트 데이터(관리자iD 추가)
+INSERT INTO users (userid, password, username)
+VALUES ('admin', '1234', '관리자');
+
+select * from users;

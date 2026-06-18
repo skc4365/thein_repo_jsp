aa21a93fd2598ed8e7f14ab4ab6b6e9860313565
skc4365 <50658047+skc4365@users.noreply.github.com>
2026-06-18 16:59:34 +0900

    예제5 추가

--- point/query.sql
+++ point/query.sql
@@ -32,3 +32,15 @@ INSERT INTO users (userid, password, username)
 VALUES ('admin', '1234', '관리자');
 
 select * from users;
+
+-- ex5 미니게시판 -------------------
+
+CREATE TABLE board (
+    id       SERIAL PRIMARY KEY,
+    title    VARCHAR(200) NOT NULL,
+    content  TEXT         NOT NULL,
+    author   VARCHAR(50)  NOT NULL,
+    reg_date TIMESTAMP    DEFAULT NOW()
+);
+select * from board;
+
